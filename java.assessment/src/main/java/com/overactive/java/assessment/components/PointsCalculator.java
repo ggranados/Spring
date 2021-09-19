132ef990b5bce037ed92ea466227840c84d75cb2
Guillermo Granados <guillermo.granados@outlook.com>
2021-09-19 20:19:24 -0300

    Added Functional Interface annotation

--- java.assessment/src/main/java/com/overactive/java/assessment/components/PointsCalculator.java
+++ java.assessment/src/main/java/com/overactive/java/assessment/components/PointsCalculator.java
@@ -2,6 +2,7 @@ package com.overactive.java.assessment.components;
 
 import java.math.BigDecimal;
 
+@FunctionalInterface
 public interface PointsCalculator {
     Long calculate(BigDecimal amount);
 }
