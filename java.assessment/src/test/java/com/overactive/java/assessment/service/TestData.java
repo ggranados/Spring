49eb48fee50114ebdc4f11f016da7851e97493a9
Guillermo Granados <guillermo.granados@outlook.com>
2021-10-01 20:40:03 -0300

    Addded unit test to controller
    TransactionController

--- java.assessment/src/test/java/com/overactive/java/assessment/service/TestData.java
+++ java.assessment/src/test/java/com/overactive/java/assessment/service/TestData.java
@@ -7,7 +7,7 @@ import java.util.Date;
 
 public class TestData {
 
-    protected static final Transaction trxApplicableForBoth;
+    public static final Transaction trxApplicableForBoth;
     static {
         trxApplicableForBoth = new Transaction(1L,"CLI001",new BigDecimal(120),new Date(),true);
     }
