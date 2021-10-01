cc1df0e8d125a2d7280820c338624263943e82b9
Guillermo Granados <guillermo.granados@outlook.com>
2021-10-01 20:40:57 -0300

    Added integration test
    Get transactions

--- java.assessment/src/test/java/com/overactive/java/assessment/ApplicationTests.java
+++ java.assessment/src/test/java/com/overactive/java/assessment/ApplicationTests.java
@@ -1,13 +1,24 @@
 package com.overactive.java.assessment;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-
+@SpringBootTest
+@AutoConfigureMockMvc
 class ApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	void getTransactions() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/transactions"))
+				.andExpect(MockMvcResultMatchers.status().isOk());
 	}
 
 }
