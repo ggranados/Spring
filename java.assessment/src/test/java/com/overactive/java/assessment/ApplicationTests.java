8dbff97326f9bf69ad1e546369c25c27c28e871c
Guillermo Granados <guillermo.granados@outlook.com>
2021-10-02 20:43:55 -0300

    Added assertion for data and meta in Json response

--- java.assessment/src/test/java/com/overactive/java/assessment/ApplicationTests.java
+++ java.assessment/src/test/java/com/overactive/java/assessment/ApplicationTests.java
@@ -4,9 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -17,8 +18,12 @@ class ApplicationTests {
 
 	@Test
 	void getTransactions() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/transactions"))
-				.andExpect(MockMvcResultMatchers.status().isOk());
+		mockMvc.perform(get("/api/v1/transactions"))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$.data").exists())
+				.andExpect(jsonPath("$.meta").exists())
+		;
 	}
 
 }
