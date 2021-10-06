c19d31ee5f007a2327ffad137aef94dead836b2b
Guillermo Granados <guillermo.granados@outlook.com>
2021-10-06 17:00:14 -0300

    Added reactive repository & controller

--- SpringGraphQL/src/main/java/com/example/springgraphql/SpringGraphQlApplication.java
+++ SpringGraphQL/src/main/java/com/example/springgraphql/SpringGraphQlApplication.java
@@ -4,6 +4,10 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 public class SpringGraphQlApplication {
@@ -11,14 +15,27 @@ public class SpringGraphQlApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringGraphQlApplication.class, args);
 
-		// Example for using record
-		var customer = new Customer(1,"Guillermo");
-		var id = customer.id();
-		var name = customer.name();
 	}
 
 }
+@Controller
+class CustomerGraphqlController{
 
+	private final CustomerRepository repository;
+
+	public CustomerGraphqlController(CustomerRepository repository) {
+		this.repository = repository;
+	}
+
+	@SchemaMapping(typeName = "Query", field = "customers")
+	Flux<Customer> customers (){
+		return this.repository.findAll();
+	}
+}
+
+interface CustomerRepository extends ReactiveCrudRepository<Customer, Integer> {
+
+}
 
 record Customer (
 		@JsonProperty("cusId") @Id Integer id,
