083bbeb7f3e18dcc128a176a10eb79d0d20e95c5
Guillermo Granados <guillermo.granados@outlook.com>
2021-10-06 16:56:07 -0300

    Added record Customer

--- SpringGraphQL/src/main/java/com/example/springgraphql/SpringGraphQlApplication.java
+++ SpringGraphQL/src/main/java/com/example/springgraphql/SpringGraphQlApplication.java
@@ -1,13 +1,27 @@
 package com.example.springgraphql;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.annotation.Id;
 
 @SpringBootApplication
 public class SpringGraphQlApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringGraphQlApplication.class, args);
+
+		// Example for using record
+		var customer = new Customer(1,"Guillermo");
+		var id = customer.id();
+		var name = customer.name();
 	}
+
+}
+
+
+record Customer (
+		@JsonProperty("cusId") @Id Integer id,
+		@JsonProperty("cusName") String name){
 
 }
