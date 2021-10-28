ab8c739762f8d24b61000f5a6a148cb9d0079978
Guillermo Granados <guillermo.granados@outlook.com>
2021-10-28 15:13:34 -0300

    Implemented get and create resevation using reservation service implementation

--- reactive-spring/src/main/java/com/linkedinlerning/reactive/controller/ReservationResource.java
+++ reactive-spring/src/main/java/com/linkedinlerning/reactive/controller/ReservationResource.java
@@ -1,6 +1,8 @@
 package com.linkedinlerning.reactive.controller;
 
 import com.linkedinlerning.reactive.model.Reservation;
+import com.linkedinlerning.reactive.service.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
@@ -12,16 +14,24 @@ public class ReservationResource {
 
     public static final String ROOM_V_1_RESERVATION = "/room/v1/reservation/";
 
-    @GetMapping(path = "{roomId}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Mono<String> getReservationById(){
-        // reservationService.getReservation(roomId);
-        return Mono.just("{}");
+    public final ReservationService reservationService;
+
+    @Autowired
+    public ReservationResource(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    @GetMapping(path = "{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<Reservation> getReservationById(@PathVariable String id){
+
+        return reservationService.getReservation(id);
     }
 
     @PostMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE,
     consumes = MediaType.APPLICATION_JSON_VALUE)
-    public Mono<String> createReservation(@RequestBody  Mono<Reservation> reservation){
-        return Mono.just("{}");
+    public Mono<Reservation> createReservation(@RequestBody  Mono<Reservation> reservation){
+
+        return reservationService.createReservation(reservation);
     }
 
     @PutMapping(path = "{roomId}", produces = MediaType.APPLICATION_JSON_VALUE,
