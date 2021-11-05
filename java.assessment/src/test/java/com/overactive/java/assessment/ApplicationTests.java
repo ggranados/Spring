7ede4d5b598d5e555b36242b6fc0be7d6a1101c0
Guillermo Granados <guillermo.granados@outlook.com>
2021-11-04 23:16:09 -0300

    added transaction app testing for coverage

--- java.assessment/src/test/java/com/overactive/java/assessment/ApplicationTests.java
+++ java.assessment/src/test/java/com/overactive/java/assessment/ApplicationTests.java
@@ -1,5 +1,6 @@
 package com.overactive.java.assessment;
 
+import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,13 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.math.BigDecimal;
+
+import static com.overactive.java.assessment.service.TestData.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.hamcrest.Matchers.*;
 
@@ -53,6 +60,31 @@ class ApplicationTests {
 				.andExpect(jsonPath("$.data[0].points").isNotEmpty())
 				.andExpect(jsonPath("$.data[0].clientId", is("CLI001")))
 				.andExpect(jsonPath("$.data[1]").doesNotExist())
+				.andExpect(jsonPath("$.meta").exists())
+				.andExpect(jsonPath("$.meta.responseTime").isNotEmpty())
+				.andExpect(jsonPath("$.meta.apiVersion", is("v1")))
+				.andExpect(jsonPath("$.meta.responseCode", is("200 OK")))
+				.andExpect(jsonPath("$.meta.errorMessage").isEmpty());
+
+	}
+
+	@Test
+	@DisplayName("Given ExistingClients When RequestRewardsByClients Then ResponseClientRewards")
+	void givenExistingClients_WhenRequestRewardsByClients_ThenResponseClientRewards() throws Exception {
+		mockMvc.perform(get("/api/v1/rewards/clients"))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$.data").exists())
+				.andExpect(jsonPath("$.data").isNotEmpty())
+				.andExpect(jsonPath("$.data[0].points").exists())
+				.andExpect(jsonPath("$.data[0].points").isNumber())
+				.andExpect(jsonPath("$.data[0].points").isNotEmpty())
+				.andExpect(jsonPath("$.data[0].clientId").isNotEmpty())
+				.andExpect(jsonPath("$.data[1]").exists())
+				.andExpect(jsonPath("$.data[1].points").exists())
+				.andExpect(jsonPath("$.data[1].points").isNumber())
+				.andExpect(jsonPath("$.data[1].points").isNotEmpty())
+				.andExpect(jsonPath("$.data[1].clientId").isNotEmpty())
 				.andExpect(jsonPath("$.meta").exists())
 				.andExpect(jsonPath("$.meta.responseTime").isNotEmpty())
 				.andExpect(jsonPath("$.meta.apiVersion", is("v1")))
@@ -132,4 +164,69 @@ class ApplicationTests {
 				.andExpect(jsonPath("$.meta.errorMessage", is("Rewards points for client: Optional[ ] not found")));
 				}
 
+	@Test
+	@DisplayName("Given Saved Transaction When Response Then Values are as Expected")
+	void givenSavedTransaction_WhenResponse_ThenValuesAreAsExpected() throws Exception {
+
+		var result = mockMvc.perform(
+					post("/api/v1/transactions")
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(asJsonString(trxDtoApplicableForBoth))
+				).andDo(print())
+				.andExpect(status().isCreated())
+				.andExpect(jsonPath("$.data").exists())
+				.andExpect(jsonPath("$.data").isNotEmpty());
+
+
+	}
+
+	@Test
+	@DisplayName("Given Edited Transaction When Response Then Values are as Expected")
+	void givenEditedTransaction_WhenResponse_ThenValuesAreAsExpected() throws Exception {
+
+		trxDtoApplicableForBoth.setAmount(BigDecimal.valueOf(1000.00));
+
+		var result = mockMvc.perform(
+						put("/api/v1/transactions")
+								.contentType(MediaType.APPLICATION_JSON)
+								.content(asJsonString(trxDtoApplicableForBoth))
+				).andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.data").exists())
+				.andExpect(jsonPath("$.data").isNotEmpty());
+
+
+	}
+
+	@Test
+	@DisplayName("Given Transaction When Get Then Values are as Expected")
+	void givenTransaction_WhenGet_ThenValuesAreAsExpected() throws Exception {
+
+		var result = mockMvc.perform(
+						get("/api/v1/transactions/1")
+								.contentType(MediaType.APPLICATION_JSON)
+								.content(asJsonString(trxDtoApplicableForBoth))
+				).andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.data").exists())
+				.andExpect(jsonPath("$.data").isNotEmpty());
+
+
+	}
+
+	@Test
+	@DisplayName("Given Transaction When Delete Then Values are as Expected")
+	void givenTransaction_WhenDelete_ThenValuesAreAsExpected() throws Exception {
+
+		var result = mockMvc.perform(
+						delete("/api/v1/transactions/1")
+								.contentType(MediaType.APPLICATION_JSON)
+								.content(asJsonString(trxDtoApplicableForBoth))
+				).andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.data").exists())
+				.andExpect(jsonPath("$.data").isEmpty());
+
+
+	}
 }
